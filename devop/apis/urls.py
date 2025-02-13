ab03553cd302573f43855409d80d85ed88371c4c
Devop-code <kamgadevop@gmail.com>
2025-02-13 19:41:24 +0100

    ajouter des vues et des URL pour la gestion des tâches, y compris la création, la suppression et la mise à jour

--- devop/apis/urls.py
+++ devop/apis/urls.py
@@ -1,1 +1,8 @@
 from django.urls import path,include
+from .views import CreateListTask , DeleteTaskView , UpdateTaskViews
+urlpatterns = [
+    
+    path('task/',CreateListTask.as_view(),name="create_task"),
+    path('task/delete/<int:pk>/',DeleteTaskView.as_view(),name="delete_task"),
+    path('task/update/<int:pk>/',UpdateTaskViews.as_view(),name="update_task"),
+]
--- devop/apis/views.py
+++ devop/apis/views.py
@@ -1,12 +1,47 @@
 from django.shortcuts import render
-from .models import CustomUser
-from rest_framework.generics import ListCreateAPIView,CreateAPIView
-from .serializers import CustomUserSerialisers
+from .models import CustomUser,Task
+from rest_framework.generics import ListCreateAPIView,CreateAPIView,DestroyAPIView,UpdateAPIView
+from .serializers import CustomUserSerialisers,TaskSerializers
 from rest_framework.permissions import IsAuthenticated , AllowAny, IsAdminUser
 # Create your views here.
 class CreateUserCustom(CreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerialisers
     permission_classes =[AllowAny]
+
+
+class DestroyUserView(DestroyAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerialisers
+    permission_classes = [IsAuthenticated ,IsAdminUser]
+class CreateListTask(ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializers
+    permission_classes=[IsAuthenticated]
     
     
+    def get_queryset(self):
+        return Task.objects.filter(author=self.request.user)
+    
+    def perform_create(self, serializer):
+        if serializer.is_valid() :
+            serializer.save(author=self.request.user)
+        else:
+            raise ValueError("mauvais donnee envoyer a l'utilisateur")
+class DeleteTaskView(DestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializers
+    permission_classes=[IsAuthenticated]
+    
+    def get_queryset(self):
+        user = self.request.user
+        return Task.objects.filter(author=user)
+
+class UpdateTaskViews(UpdateAPIView):
+    queryset = Task.objects.all()
+    serializer_class=TaskSerializers
+    permission_classes=[IsAuthenticated]
+    
+    def get_queryset(self):
+        user = self.request.user
+        return Task.objects.filter(author=user)
